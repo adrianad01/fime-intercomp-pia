8d86581b3cb19f2c7cb09cad17ac3d90b1b75a7f
Adrian Alcocer <jadrianaldo@outlook.com>
2024-11-06 01:14:11 -0600

    feat: add form to generate pdf

--- src/router/app-router.jsx
+++ src/router/app-router.jsx
@@ -4,37 +4,18 @@ import { Tramites } from '../pages/Tramites';
 import { TramitePage } from '../pages/TramitePage';
 import { TramitePage2 } from '../pages/TramitePage2';
 import { TramitePage3 } from '../pages/TramitePage3';
-
+import PaymentFormPage from "../pages/PaymentFormPage";  // Página de pago
 export const AppRouter = () => {
-
-    return (
-        <BrowserRouter>
-
-            <Routes>
-                <Route
-                    index
-                    element={ <Home /> }
-                />
-                <Route 
-                    path='/mis-tramites'
-                    element={ <Tramites /> }
-                />
-                <Route 
-                    path='/tramite-licenciatura'
-                    element={ <TramitePage /> }
-                />
-                <Route 
-                    path='/tramite-doctorado'
-                    element={ <TramitePage2 /> }
-                />
-                <Route 
-                    path='/tramite-programa-tecnico'
-                    element={ <TramitePage3 /> }
-                />
-            </Routes>
-
-        </BrowserRouter>
-    );
-
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<Home />} />
+        <Route path='/mis-tramites' element={<Tramites />} />
+        <Route path='/tramite-licenciatura' element={<TramitePage />} />
+        <Route path='/tramite-doctorado' element={<TramitePage2 />} />
+        <Route path='/tramite-programa-tecnico' element={<TramitePage3 />} />
+        <Route path="/payment" element={<PaymentFormPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
-
